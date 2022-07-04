b8e7f620a343e766241c3985388dc2624f2f74d0
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-07-04 12:01:57 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 484 ms, Memory: 47248 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#define SIZE 1000
+
+using namespace std;
+
+bool map[SIZE][SIZE];
+int visited[11][SIZE][SIZE] = {false};
+queue<pair<int, int>> q;
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void solution() {
+    int N, M, K;
+    int x, y, k, nx, ny, dist, result = -1;
+    string s;
+    
+    cin >> N >> M >> K;
+    for (y = 0; y < N; ++y) {
+        cin >> s;
+        for (x = 0; x < M; ++x) {
+            map[y][x] = s[x] == '1';
+            for (k = 0; k <= K; ++k)
+                visited[k][y][x] = 0;
+        }
+    }
+
+    visited[0][0][0] = 1;
+    q.emplace(0, 100);
+    while (!q.empty()) {
+        x = q.front().first/SIZE;
+        y = q.front().first%SIZE;
+        k = q.front().second%100;
+        dist = q.front().second/100;
+        q.pop();
+
+        if (dist > visited[k][y][x]) continue;
+        if (x == M-1 && y == N-1) {
+            result = dist;
+            break;
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if (0 <= nx && nx < M && 0 <= ny && ny < N) {
+                if (!map[ny][nx] && !visited[k][ny][nx]) {
+                    visited[k][ny][nx] = dist + 1;
+                    q.emplace(nx*SIZE+ny, visited[k][ny][nx]*100 + k);
+                }
+                else if (map[ny][nx] && k < K && !visited[k+1][ny][nx]) {
+                    visited[k+1][ny][nx] = dist + 1;
+                    q.emplace(nx*SIZE+ny, visited[k+1][ny][nx]*100 + k+1);
+                }
+            }
+        }
+    }
+
+    cout << result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
