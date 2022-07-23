c9ae3f5746662c089fe6f05873fefdf11929cd5e
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-07-23 12:57:36 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    cout << ".  .   .\n"
+            "|  | _ | _. _ ._ _  _\n"
+            "|/\\|(/.|(_.(_)[ | )(/.";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
