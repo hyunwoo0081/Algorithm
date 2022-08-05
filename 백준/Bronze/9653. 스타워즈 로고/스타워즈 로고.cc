b237326460bd3a618d7f80eb7beccd0805ea49af
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-08-05 15:35:20 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void solution() {
+    cout << "    8888888888  888    88888\n"
+            "   88     88   88 88   88  88\n"
+            "    8888  88  88   88  88888\n"
+            "       88 88 888888888 88   88\n"
+            "88888888  88 88     88 88    888888\n"
+            "\n"
+            "88  88  88   888    88888    888888\n"
+            "88  88  88  88 88   88  88  88\n"
+            "88 8888 88 88   88  88888    8888\n"
+            " 888  888 888888888 88  88      88\n"
+            "  88  88  88     88 88   88888888";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
