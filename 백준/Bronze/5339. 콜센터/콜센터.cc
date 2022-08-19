fbb84dabe89275338a27b17685a1a47c56c24606
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-08-19 13:31:02 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    cout << "     /~\\\n"
+            "    ( oo|\n"
+            "    _\\=/_\n"
+            "   /  _  \\\n"
+            "  //|/.\\|\\\\\n"
+            " ||  \\ /  ||\n"
+            "============\n"
+            "|          |\n"
+            "|          |\n"
+            "|          |";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
