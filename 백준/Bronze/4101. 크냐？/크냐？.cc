b1467a98d82293280ef6a876ac6a34596751549d
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-08-21 23:20:00 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int a, b;
+
+    cin >> a >> b;
+    while (!(a == 0 && b == 0)) {
+        cout << ( a > b ? "Yes\n" : "No\n");
+        cin >> a >> b;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
