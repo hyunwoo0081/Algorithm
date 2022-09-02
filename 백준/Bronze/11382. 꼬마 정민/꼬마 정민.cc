5f90a891b5521f9b9dddccaf0f548a24993f946c
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-09-02 11:50:24 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a+b+c;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
