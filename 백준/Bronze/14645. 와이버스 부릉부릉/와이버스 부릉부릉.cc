4cebf3f5c95806515e316b0e82739f1131480a96
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-09-03 13:05:00 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    cout << "비와이";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
