ee883602775630170e308e632854c9135b180eaf
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-10-21 11:12:20 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a > b) swap(a, b);
+    if (b > c) swap(b, c);
+    if (a > b) swap(a, b);
+
+    cout << a << ' ' << b << ' ' << c;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+
+    return 0;
+}
