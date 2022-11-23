cef7d6d1112a4bce7de76498a694ea8c243d52b3
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-11-23 19:38:09 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    string s;
+    cin >> s;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << s << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
