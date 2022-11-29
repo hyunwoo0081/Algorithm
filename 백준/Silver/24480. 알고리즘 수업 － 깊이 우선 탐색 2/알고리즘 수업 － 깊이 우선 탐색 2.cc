c149d1f3b138108ce2d5df9c2ed1f313bdb4f84b
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2022-11-29 12:39:27 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 92 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define SIZE 100'000
+
+using namespace std;
+
+vector<int> edges[SIZE];
+int dist[SIZE];
+int visitedCount = 0;
+
+void dfs(int start) {
+    dist[start] = ++visitedCount;
+
+    for (auto next: edges[start])
+        if (!dist[next])
+            dfs(next);
+}
+
+void solution() {
+    int N, M, R, u, v;
+
+    cin >> N >> M >> R;
+    while (M--) {
+        cin >> u >> v;
+        u--, v--;
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+
+    for (int i = 0; i < N; i++)
+        sort(edges[i].begin(), edges[i].end(), greater<>());
+
+    dfs(--R);
+
+    for (int i = 0; i < N; i++)
+        cout << dist[i] << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
