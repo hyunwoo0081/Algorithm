8765ec3adac1b1e621765207ff8af16b31122545
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2023-01-17 12:27:22 +0900

    [Gold IV] Title: 작업, Time: 48 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.cc
+++ 백준/Gold/2056. 작업/작업.cc
@@ -1,47 +1,25 @@
 #include <iostream>
-#include <queue>
 #define SIZE 10000
 
 using namespace std;
 
-queue<int> q;
-vector<int> edges[SIZE];
-int times[SIZE];
 int dist[SIZE] = {0};
-int indeg[SIZE] = {0};
 
 void solution() {
-    int N, K, v, result;
+    int N, K, v, t, result = 0;
 
     cin >> N;
     for (int i = 0; i < N; i++) {
-        cin >> times[i] >> K;
-        dist[i] = times[i];
+        cin >> t >> K;
         while (K--) {
             cin >> v;
-            edges[i].push_back(v-1);
-            indeg[v-1]++;
+            v--;
+            dist[i] = max(dist[i], dist[v]);
         }
-    }
-
-    for (int i = 0; i < N; i++)
-        if (indeg[i] == 0)
-            q.push(i);
-
-    while (!q.empty()) {
-        v = q.front();
-        q.pop();
-
-        for (auto u : edges[v]) {
-            dist[u] = max(dist[u], dist[v]+times[u]);
-            if (--indeg[u] == 0)
-                q.push(u);
-        }
-    }
-
-    result = 0;
-    for (int i = 0; i < N; i++)
+        dist[i] += t;
         result = max(result, dist[i]);
+    }
+    
     cout << result;
 }
 
