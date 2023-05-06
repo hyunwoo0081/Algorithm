7828c6d1e42d6c173c8561f768273e1368b366f2
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2023-05-06 23:52:04 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    long long N;
+    cin >> N;
+    cout << N*N*N << '\n' << 3;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
