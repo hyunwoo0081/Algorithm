366c5fa92e05f776095ed798f7fad1d9a8b6b43a
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2023-05-18 00:07:44 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void solution() {
+    int arr[3];
+    for (auto &a : arr)
+        cin >> a;
+    sort(arr, arr+3);
+    cout << arr[1];
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
