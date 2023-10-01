191b0de00b8e965a335ee1cad93e4f59805bac1c
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2023-10-01 21:01:37 +0900

    [Bronze V] Title: Арифметическая магия, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int n;
+    cin >> n;
+    cout << 1;
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
