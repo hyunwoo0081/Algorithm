fd086a632a61de571d902fc7789b0e62c889f91f
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2023-10-07 13:03:10 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int B;
+    cin >> B;
+    B = B * 10 / 11;
+    cout << B;
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
