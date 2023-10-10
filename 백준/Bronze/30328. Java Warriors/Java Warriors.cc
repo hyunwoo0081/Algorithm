0b4343a175771d5629679a20973242334631dbf1
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2023-10-10 19:25:46 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int n;
+    cin >> n;
+    cout << 4000 * n;
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
