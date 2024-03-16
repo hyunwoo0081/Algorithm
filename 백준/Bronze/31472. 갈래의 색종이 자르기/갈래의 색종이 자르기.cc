0907820ca6b197c8bee9defc62683d1c7752fdfb
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2024-03-16 17:36:23 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void solution() {
+    int N;
+
+    cin >> N;
+    cout << sqrt(N*2)*4;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
