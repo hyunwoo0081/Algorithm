03d87b5e387bef621cc92ea175b4f39b350aa590
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2024-05-14 21:07:35 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void solution() {
+  int A, B;
+  cin >> A >> B;
+  cout << A + B + 3;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  solution();
+}
