b32a5a4149feb959518fe78b31db401752b5413e
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2024-10-24 14:00:39 +0900

    [Silver I] Title: 물수제비 (SUJEBI), Time: 120 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Silver/14931. 물수제비 （SUJEBI）/물수제비 （SUJEBI）.cc
+++ 백준/Silver/14931. 물수제비 （SUJEBI）/물수제비 （SUJEBI）.cc
@@ -14,39 +14,28 @@ void solution() {
         cin >> arr[i];
     }
 
-    // for (d = L; d > L/2; d--) {
-    //     prefix[d-1] = arr[d-1];
-    //
-    //     if (prefix[d-1] >= m) {
-    //         m = prefix[d-1];
-    //         mi = d;
-    //     }
-    // }
-    //
-    // for (; d > 0; d--) {
-    //     prefix[d-1] = prefix[d*2-1];
-    //     for (i = d-1; i < L; i += d*2) {
-    //         prefix[d-1] += arr[i];
-    //     }
-    //
-    //     if (prefix[d-1] >= m) {
-    //         m = prefix[d-1];
-    //         mi = d;
-    //     }
-    // }
+    for (d = L; d > L/2; d--) {
+        prefix[d-1] = arr[d-1];
 
-    for (d = 1; d <= L; d++) {
-        prefix[d-1] = 0;
-        for (i = d-1; i < L; i += d)
-            prefix[d-1] += arr[i];
-
-        if (prefix[d-1] > m) {
+        if (prefix[d-1] >= m) {
             m = prefix[d-1];
             mi = d;
         }
     }
 
-    // if (m == 0) mi = 0;
+    for (; d > 0; d--) {
+        prefix[d-1] = prefix[d*2-1];
+        for (i = d-1; i < L; i += d*2) {
+            prefix[d-1] += arr[i];
+        }
+
+        if (prefix[d-1] >= m) {
+            m = prefix[d-1];
+            mi = d;
+        }
+    }
+
+    if (m == 0) mi = 0;
 
     cout << mi << ' ' << m;
 }
