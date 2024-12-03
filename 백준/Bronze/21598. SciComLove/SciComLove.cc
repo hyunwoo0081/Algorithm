76fab7f1376986c267cd6993a95555c963e7a2c5
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2024-12-03 10:07:41 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int N;
+    cin >> N;
+    while (N--) {
+        cout << "SciComLove\n";
+    }
+}
+
+int main(){
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    solution();
+
+    return 0;
+}
