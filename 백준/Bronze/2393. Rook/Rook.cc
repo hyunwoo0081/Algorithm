fc92f0b4275ffc35224e21eae474b73a5f27f01a
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2025-01-29 15:12:36 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    cout <<
+"  ___  ___  ___\n" <<
+"  | |__| |__| |\n" <<
+"  |           |\n" <<
+"   \\_________/\n" <<
+"    \\_______/\n" <<
+"     |     |\n" <<
+"     |     |\n" <<
+"     |     |\n" <<
+"     |     |\n" <<
+"     |_____|\n" <<
+"  __/       \\__\n" <<
+" /             \\\n" <<
+"/_______________\\";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
