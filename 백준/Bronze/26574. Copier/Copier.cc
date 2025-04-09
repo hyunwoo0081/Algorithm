02c21e7abad6629ee2819b3c2780de54a8c40c73
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2025-04-09 20:18:49 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T, N;
+    
+    cin >> T;
+    while (T--) {
+        cin >> N;
+        cout << N << ' ' << N << '\n';
+    }
+
+    return 0;
+}
