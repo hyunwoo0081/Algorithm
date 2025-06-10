27b78d270a5c7abe04486d101874a2976700f319
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2025-06-10 13:13:04 +0900

    [Bronze I] Title: 세준세비, Time: 184 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.cc
+++ 백준/Bronze/1524. 세준세비/세준세비.cc
@@ -1,33 +1,25 @@
 #include <iostream>
-#include <algorithm>
-#define SIZE 1'000'000
 
 using namespace std;
 
-int srr[SIZE], brr[SIZE];
-
 int main() {
-    int T, N, M, i, s, b;
+    int T, N, M, i, a, s, b;
     
     cin >> T;
-    while (T-- ) {
+    while (T--) {
         cin >> N >> M;
-        for (i = 0; i < N; i++)
-            cin >> srr[i];
-            
-        for (i = 0; i < M; i++)
-            cin >> brr[i];
-            
-        sort(srr, srr+N);
-        sort(brr, brr+M);
-        
         s = b = 0;
-        while (s < N && b < M) {
-            if (srr[s] < brr[b]) s++;
-            else b++;
+        for (i = 0; i < N; i++) {
+            cin >> a;
+            s = max(a, s);
+        }
+            
+        for (i = 0; i < M; i++) {
+            cin >> a;
+            b = max(a, b);
         }
         
-        cout << (s == N ? 'B' : 'S') << '\n';
+        cout << (s < b ? 'B' : 'S') << '\n';
     }
 
     return 0;
