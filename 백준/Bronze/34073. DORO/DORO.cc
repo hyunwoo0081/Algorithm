a1e26980518b9b2cd9118f581e899d3ad9d0cf7d
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2025-08-24 19:32:23 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+    int N;
+    string s;
+
+    cin >> N;
+    while (N--) {
+        cin >> s;
+        cout << s << "DORO ";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solution();
+
+    return 0;
+}
