fcfac7731bc009026fc870ba58cfac7008591b7f
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2026-03-20 21:10:28 +0900

    [Bronze II] Title: 수 이어 쓰기 3, Time: 4 ms, Memory: 2752 KB -BaekjoonHub

--- 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.cc
+++ 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.cc
@@ -1,51 +1,17 @@
 #include <iostream>
-#include <deque>
 
 using namespace std;
 
-deque<char> v;
-
-bool startWith(string &s) {
-    bool result = true;
-    for (int i = 0; i < s.length(); i++) {
-        result &= v[i] == s[i];
-    }
-    
-    return result;
-}
-
-int stackSize = 0;
-char stack[10];
-
 int main() {
-    string s;
-    cin >> s;
+    int n;
+    string t;
     
-    for (int i = 1; i < 10; i++) {
-        v.push_back(i+'0');
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        t += to_string(i);
     }
     
-    int next = 10;
-    int result = 1;
-    while (!startWith(s)) {
-        result++;
-        v.pop_front();
-        
-        if (v.size() < s.length()) {
-            int tmp = next++;
-            
-            stackSize = 0;
-            while (tmp > 0) {
-                stack[stackSize++] = tmp % 10;
-                tmp /= 10;
-            }
-            
-            while (stackSize > 0) {
-                v.push_back(stack[--stackSize] + '0');
-            }
-        }
-    }
-    cout << result;
+    cout << t.find(to_string(n)) + 1;
 
     return 0;
 }
